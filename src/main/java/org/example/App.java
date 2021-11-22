a99c143ee81e218ff1ae94f8762b1ee551de5ccc
Dmitry <Xranitellll@mail.ru>
2021-11-22 12:05:56 +0300

    Variables

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,6 +10,10 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
+        String login = "Testtest15";
+        String password = "ghbdtn13";
+        String firstSearchValue = "Eldritch knight / Рыцарь Элдрича";
+        String secondSearchValue = "Skyrim Exterior Fixes / Внешние исправления Skyrim LE";
 
         WebDriverManager.chromedriver().setup();
 
@@ -19,30 +23,29 @@ public class App {
 
         driver.get("https://gamer-mods.ru");
         driver.findElement(By.xpath("//div[@class=\"header-right\"]/a[@href=\"javascript:;\"]")).click();
-        driver.findElement(By.name("user")).sendKeys("Testtest15");
-        driver.findElement(By.name("password")).sendKeys("ghbdtn13");
+        driver.findElement(By.name("user")).sendKeys(login);
+        driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.id("subbutfrmLg555")).click();
         driver.findElement(By.xpath("//a[@class=\"profile-view pv-button\"]")).click();
         driver.findElement(By.xpath("//a[text()='Выход']")).click();
         Thread.sleep(5000);
         driver.quit();
 
-
         // 2 - Find mods
         WebDriver driverSecondTest = new ChromeDriver();
         driverSecondTest.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driverSecondTest.get("https://gamer-mods.ru");
         driverSecondTest.findElement(By.xpath("//div[@class=\"header-right\"]/a[@href=\"javascript:;\"]")).click();
-        driverSecondTest.findElement(By.name("user")).sendKeys("Testtest15");
-        driverSecondTest.findElement(By.name("password")).sendKeys("ghbdtn13");
+        driverSecondTest.findElement(By.name("user")).sendKeys(login);
+        driverSecondTest.findElement(By.name("password")).sendKeys(password);
         driverSecondTest.findElement(By.id("subbutfrmLg555")).click();
         WebElement searchLine = driverSecondTest.findElement(By.name("query"));
-        searchLine.sendKeys("Eldritch knight / Рыцарь Элдрича");
+        searchLine.sendKeys(firstSearchValue);
         WebElement searchButton = driverSecondTest.findElement(By.xpath("//form[@id=\"search\"]/input[@name=\"a\"]"));
         searchButton.click();
         Thread.sleep(5000);
         WebElement searchLineAgain = driverSecondTest.findElement(By.name("query"));
-        searchLineAgain.sendKeys("Skyrim Exterior Fixes / Внешние исправления Skyrim LE");
+        searchLineAgain.sendKeys(secondSearchValue);
         WebElement searchButtonAgain = driverSecondTest.findElement(By.xpath("//form[@id=\"search\"]/input[@name=\"a\"]"));
         searchButtonAgain.click();
         Thread.sleep(5000);
